98aaebb1495ba5239d29cc636e4e84fd3b9a41d6
Vjekoni <dizdar.vjeko@gmail.com>
2021-04-15 17:00:21 +0200

    commited for checkbox testing

---
+++ app.js
@@ -0,0 +1,42 @@
+// const slider = document.querySelector("#slider");
+// const annually = document.querySelector(".annually");
+// const monthly = document.querySelector(".monthly");
+// const lowPrice = document.querySelectorAll(".lower-price");
+// const bigPrice = document.querySelectorAll(".bigger-price");
+
+// function check() {
+
+//       if (slider.checked == true) {
+//             monthly.classList.add("active")
+//             annually.classList.remove("active")
+//             lowerPrice()
+
+//       } else {
+//             annually.classList.add("active")
+//             monthly.classList.remove("active")
+//             biggerPrice()
+//       }
+
+// }
+
+// function lowerPrice() {
+//       for (let i = 0; i < lowPrice.length; i++) {
+//             lowPrice[i].style.display = "block";
+//       }
+
+//       for (let i = 0; i < bigPrice.length; i++) {
+//             bigPrice[i].style.display = "none";
+//       }
+// }
+
+// function biggerPrice() {
+//       for (let i = 0; i < bigPrice.length; i++) {
+//             bigPrice[i].style.display = "block";
+//       }
+
+//       for (let i = 0; i < lowPrice.length; i++) {
+//             lowPrice[i].style.display = "none";
+//       }
+// }
+
+// slider.addEventListener("change", check)
