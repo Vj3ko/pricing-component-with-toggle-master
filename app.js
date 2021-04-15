17e0b22b3c151232114fd94dced44c0497857124
Vjekoni <dizdar.vjeko@gmail.com>
2021-04-15 19:55:29 +0200

    fixed some issues with checkbox

--- app.js
+++ app.js
@@ -1,42 +1,42 @@
-// const slider = document.querySelector("#slider");
-// const annually = document.querySelector(".annually");
-// const monthly = document.querySelector(".monthly");
-// const lowPrice = document.querySelectorAll(".lower-price");
-// const bigPrice = document.querySelectorAll(".bigger-price");
+const slider = document.querySelector("#checkbox");
+const annually = document.querySelector(".annually");
+const monthly = document.querySelector(".monthly");
+const lowPrice = document.querySelectorAll(".lower-price");
+const bigPrice = document.querySelectorAll(".bigger-price");
 
-// function check() {
+function check() {
 
-//       if (slider.checked == true) {
-//             monthly.classList.add("active")
-//             annually.classList.remove("active")
-//             lowerPrice()
+      if (slider.checked == true) {
+            monthly.classList.add("active")
+            annually.classList.remove("active")
+            lowerPrice()
 
-//       } else {
-//             annually.classList.add("active")
-//             monthly.classList.remove("active")
-//             biggerPrice()
-//       }
+      } else {
+            annually.classList.add("active")
+            monthly.classList.remove("active")
+            biggerPrice()
+      }
 
-// }
+}
 
-// function lowerPrice() {
-//       for (let i = 0; i < lowPrice.length; i++) {
-//             lowPrice[i].style.display = "block";
-//       }
+function lowerPrice() {
+      for (let i = 0; i < lowPrice.length; i++) {
+            lowPrice[i].style.display = "block";
+      }
 
-//       for (let i = 0; i < bigPrice.length; i++) {
-//             bigPrice[i].style.display = "none";
-//       }
-// }
+      for (let i = 0; i < bigPrice.length; i++) {
+            bigPrice[i].style.display = "none";
+      }
+}
 
-// function biggerPrice() {
-//       for (let i = 0; i < bigPrice.length; i++) {
-//             bigPrice[i].style.display = "block";
-//       }
+function biggerPrice() {
+      for (let i = 0; i < bigPrice.length; i++) {
+            bigPrice[i].style.display = "block";
+      }
 
-//       for (let i = 0; i < lowPrice.length; i++) {
-//             lowPrice[i].style.display = "none";
-//       }
-// }
+      for (let i = 0; i < lowPrice.length; i++) {
+            lowPrice[i].style.display = "none";
+      }
+}
 
-// slider.addEventListener("change", check)
+slider.addEventListener("change", check)
